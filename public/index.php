0e1eecbd6a14b89936d9ab8d2b369ef49ca501c6
Arviu <arviu23@gmail.com>
2026-03-31 22:46:47 +0200

    created basic navbar

--- public/index.php
+++ public/index.php
@@ -1,2 +1,25 @@
-<?php
-echo "Hello World";
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Arviu.net</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" type="image/png" href="assets/images/Ana logo.ico">
+        <link rel="stylesheet" href="assets/css/style.css">
+    </head>
+    <body class="body">
+        <h1>Test</h1>
+        <header class="header"></header>
+        <nav class="nav">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="blog.php">Blog</a></li>
+            </ul>
+        </nav>
+        <main class="main"></main>
+        <footer class="footer"></footer>
+        <script src="assets/js/script.js" defer></script>
+    </body>
+</html>
