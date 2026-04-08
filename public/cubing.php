14e2d0798e592131a16714ef500569fafb43bf04
Arviu <arviu23@gmail.com>
2026-04-08 22:15:38 +0200

    Added cubing page and pr table

---
+++ public/cubing.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Cubing</title>
+  <link rel="icon" type="image/png" href="assets/images/arviu-logo.ico">
+  <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="stylesheet" href="assets/css/cubing.css">
+
+</head>
+<body>
+
+  <div id="navbar"></div>
+  <header class="header">
+    <h1>Welcome to Arviu.net</h1>
+    <p></p>
+  </header>
+
+  <main class="main-content">
+    <section>
+        <h2>My speedcubing PRs</h2>
+        <br>
+       <table class="pr-table">
+        <tr>
+            <th>Event</th>
+            <th>Single</th>
+            <th>ao5</th>
+            <th>ao12</th>
+            <th>ao100</th>
+            <th>ao1000</th>
+        </tr>
+        <tr>
+            <td class="event">3x3</td>
+            <td>8.72</td>
+            <td>11.29</td>
+            <td>11.87</td>
+            <td>12.97</td>
+            <td>13.55</td>
+        </tr>
+        <tr>
+            <td class="event">4x4</td>
+            <td>1:21.62</td>
+            <td>1:34.27</td>
+            <td>1:39.58</td>
+            <td>2:18.86</td>
+            <td>-</td>
+        </tr>
+       </table>
+    </section>
+  </main>
+  <div id="footer"></div>
+  <script type="module" src="assets/js/index.js" defer></script>
+</body>
+</html>
